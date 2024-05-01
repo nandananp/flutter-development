419ad4ed8bad25f8aa72ddbfd2b5051712bdc84e
shanuNP <shanunp2016@gmail.com>
2024-05-01 13:10:38 +0530

    feat: calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,94 @@
-import 'dart:math';
+// ignore_for_file: avoid_unnecessary_containers
 
+import 'package:calculator_app/buttons.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Calculatorapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Calculatorapp extends StatelessWidget {
+  //constructor
+  const Calculatorapp({Key? key}) : super(key: key);
+
+//build function
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Navigation Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const FirstPage(title: 'FirstPage'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //calling homepage state
+      home: HomePage(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key, required this.title}) : super(key: key);
-  final String title;
+//definition of homePageState function
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    '*',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () async {
-            var message = await Navigator.push(context,
-                MaterialPageRoute(builder: (context) {
-              return const SecondPage(title: 'SecondPage');
-            }));
-            print(message); // Changed 'log' to 'print' here
-          },
-          child: const Text('Next'),
-        ),
+      backgroundColor: Colors.deepPurple[100],
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    return MyButton(
+                      buttontext: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? Colors.green
+                          : Colors.deepPurple[50],
+                      textColor: isOperator(buttons[index])
+                          ? Colors.green
+                          : Colors.deepPurple,
+                    );
+                  }),
+            ),
+          ),
+        ],
       ),
     );
   }
-}
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.pop(context, "Returned from SecondPage");
-          },
-          child: const Text('Go Back'),
-        ),
-      ),
-    );
+  bool isOperator(String x) {
+    List<String> operators = ['%', '/', '*', '+', '-', '='];
+    return operators.contains(x);
   }
 }
